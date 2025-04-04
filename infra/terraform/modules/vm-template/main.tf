0d727e98e01db8e71939cd757fc9afbfeb25a0a1
RawSalmon <phanthawasjira@Gmail.com>
2025-04-04 09:36:53 +0700

    Update main.tf

--- infra/terraform/modules/vm-template/main.tf
+++ infra/terraform/modules/vm-template/main.tf
@@ -4,20 +4,15 @@ resource "proxmox_vm_qemu" "template" {
   desc        = "Ubuntu 22.04 LTS template for Terraform deployments"
   target_node = var.proxmox_node
 
-  # Clone from your existing template
-  clone       = "ubuntu-2204-template"  # or var.base_template
   // Clone from the existing VM with ID 9000
   clone       = "ubuntu-2204-template"  // Directly reference the VM ID
   full_clone  = true
 
-  # Template settings
   // Basic VM configuration
   cores       = 2
   sockets     = 1
   memory      = 2048
-  onboot      = false  # Templates shouldn't auto-start
 
-  # Cloud-init settings
   // Ensure it doesn't auto-start (it's meant to be a template)
   onboot      = false
 
@@ -27,14 +22,12 @@ resource "proxmox_vm_qemu" "template" {
   ipconfig0   = "ip=dhcp"
   sshkeys     = join("\n", var.ssh_keys)
 
-  # Network interfaces
   // Network interface
   network {
     model     = "virtio"
     bridge    = "vmbr0"
   }
 
-  agent = 1
   // Enable qemu-guest-agent
   agent       = 1
 
@@ -42,7 +35,6 @@ resource "proxmox_vm_qemu" "template" {
   oncreate    = false
 }
 
-# Convert the VM to a template after creation
 // Convert VM to template after creation
 resource "null_resource" "convert_to_template" {
   depends_on = [proxmox_vm_qemu.template]
